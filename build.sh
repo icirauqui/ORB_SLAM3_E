a2da89e59180323a65d3a8b7048f2d6dd6a32bbc
icirauqui <icirauqui@gmail.com>
2021-07-14 20:17:54 +0200

    init3

---
+++ build.sh
@@ -0,0 +1,80 @@
+echo "                               "
+echo "Configuring and building Thirdparty/DBoW2 ..."
+cd Thirdparty/DBoW2
+case $1 in 0) 
+	mkdir build
+	;;
+esac
+cd build
+case $1 in 0) 
+	cmake .. -DCMAKE_BUILD_TYPE=Release
+	;;
+esac
+make -j
+
+
+
+
+echo "                               "
+echo "Configuring and building Thirdparty/g2o ..."
+cd ../../g2o
+case $1 in 0) 
+	mkdir build
+	;;
+esac
+cd build
+case $1 in 0) 
+	cmake .. -DCMAKE_BUILD_TYPE=Release
+	;;
+esac
+make -j
+
+cd ../../../
+
+
+
+
+case $1 in 0) 
+	echo "                               "
+	echo "Uncompress vocabulary ..."
+	cd Vocabulary
+	tar -xf ORBvoc.txt.tar.gz
+	cd ..
+	;;
+esac
+
+
+
+
+echo "                               "
+echo "Configuring and building ORB_SLAM3 ..."
+case $1 in 0) 
+	mkdir build
+	;;
+esac
+cd build
+case $1 in 0) 
+	cmake .. -DCMAKE_BUILD_TYPE=Release
+	;;
+esac
+make -j
+
+
+
+
+echo "                               "
+echo "Configuring and building ROS examples ..."
+cd ../Examples/ROS/ORB_SLAM3_E
+case $1 in 0) 
+	mkdir build
+	;;
+esac
+cd build
+case $1 in 0) 
+	cmake .. -DROS_BUILD_TYPE=Release
+	;;
+esac
+make
+
+echo "                               "
+
